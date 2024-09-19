7ae9aeea491c0953e3d919abb6e1e0dd87279c4c
junbeom-Son <junbeom247@gmail.com>
2024-09-19 16:49:52 +0900

    feat: access kakao login

--- backend/user/src/main/java/io/ssafy/p/j11a307/user/controller/LoginController.java
+++ backend/user/src/main/java/io/ssafy/p/j11a307/user/controller/LoginController.java
@@ -1,13 +1,28 @@
 package io.ssafy.p.j11a307.user.controller;
 
+import io.ssafy.p.j11a307.user.util.KakaoUtil;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.net.URI;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/login")
 public class LoginController {
 
+    private final KakaoUtil kakaoUtil;
 
+    @GetMapping("/kakao")
+    public ResponseEntity<Void> kakaoLogin() {
+        String uri = kakaoUtil.createKakaoRedirecUri();
+        HttpHeaders headers = new HttpHeaders();
+        headers.setLocation(URI.create(uri));
+        return new ResponseEntity<>(headers, HttpStatus.MOVED_PERMANENTLY);
+    }
 }
