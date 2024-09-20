295d9e7b7f843b0b1cf524ce8c6a736c623c4bc0
junbeom-Son <junbeom247@gmail.com>
2024-09-20 15:01:57 +0900

    feat: get kakao login url

--- backend/user/src/main/java/io/ssafy/p/j11a307/user/util/KakaoUtil.java
+++ backend/user/src/main/java/io/ssafy/p/j11a307/user/util/KakaoUtil.java
@@ -1,11 +1,18 @@
 package io.ssafy.p.j11a307.user.util;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
 public class KakaoUtil {
 
+    @Value("${kakao.redirect-uri}")
+    private String redirectUri;
+
+    @Value("${kakao.app-key}")
+    private String appKey;
+
     public String createKakaoRedirecUri() {
-        return "";
+        return "https://kauth.kakao.com/oauth/authorize?response_type=code&client_id=" + appKey + "&redirect_uri=" + redirectUri;
     }
 }
