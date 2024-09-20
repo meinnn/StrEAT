61d8961345c456a677a327f6b4a1ec4f95f8fd61
junbeom-Son <junbeom247@gmail.com>
2024-09-20 15:17:42 +0900

    feat: add repository dependency on login service

--- backend/user/src/main/java/io/ssafy/p/j11a307/user/service/LoginService.java
+++ backend/user/src/main/java/io/ssafy/p/j11a307/user/service/LoginService.java
@@ -1,7 +1,12 @@
 package io.ssafy.p.j11a307.user.service;
 
+import io.ssafy.p.j11a307.user.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class LoginService {
+
+    private final UserRepository userRepository;
 }
