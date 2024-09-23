c57fadd88fcc5c1675330674bc90e6a0b0ce3a6b
junbeom-Son <junbeom247@gmail.com>
2024-09-23 15:10:29 +0900

    fix: remove annotation

--- backend/eureka/src/test/java/io/ssafy/p/j11a307/eureka/EurekaApplicationTests.java
+++ backend/eureka/src/test/java/io/ssafy/p/j11a307/eureka/EurekaApplicationTests.java
@@ -1,9 +1,7 @@
 package io.ssafy.p.j11a307.eureka;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class EurekaApplicationTests {
 
 	@Test
