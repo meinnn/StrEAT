67a149dc32e7243f8750f03eb0f81a5ec0b0ebf2
김민수 <ch05036@naver.com>
2024-09-23 15:37:35 +0900

    feat: 뒤로 가기 버튼 생성

--- front/src/containers/menu-add/index.tsx
+++ front/src/containers/menu-add/index.tsx
@@ -2,14 +2,18 @@
 
 import MenuAddDetails from "./MenuAddDetails"
 import MenuAddOption from "./MenuAddOption"
+import { FaArrowLeft } from "react-icons/fa";
 
 const MenuAdd = () => {
   return (
     <div>
       <div className="max-w-xl mx-auto flex flex-col">
         {/* 메뉴 추가 타이틀 */}
-        <div className="mb-6 mt-4 w-full">
-          <h1 className="text-2xl font-bold text-primary-950 text-center">메뉴 추가</h1>
+        <div className="relative mb-6 mt-4 w-full flex items-center"> {/* relative 위치 지정 */}
+          <button className="text-xl text-primary-950 absolute left-4"> {/* 버튼을 왼쪽에 고정 */}
+            <FaArrowLeft />
+          </button>
+          <h1 className="text-2xl font-bold text-primary-950 mx-auto">메뉴 추가</h1> {/* 중앙 정렬 */}
         </div>
 
         {/* 메뉴 세부 정보 */}
