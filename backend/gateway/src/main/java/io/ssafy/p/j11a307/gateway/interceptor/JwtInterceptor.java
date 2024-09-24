29049faac76373048407e00a8072a7ab3d396b9c
junbeom-Son <junbeom247@gmail.com>
2024-09-24 14:32:04 +0900

    feat: check the requested token is valid

---
+++ backend/gateway/src/main/java/io/ssafy/p/j11a307/gateway/interceptor/JwtInterceptor.java
@@ -0,0 +1,27 @@
+package io.ssafy.p.j11a307.gateway.interceptor;
+
+import io.ssafy.p.j11a307.gateway.util.JwtUtil;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import org.springframework.web.server.WebFilter;
+import org.springframework.web.server.WebFilterChain;
+import reactor.core.publisher.Mono;
+
+@RequiredArgsConstructor
+@Component
+public class JwtInterceptor implements WebFilter {
+
+    private final String HEADER_AHTU = "Authorization";
+    private final JwtUtil jwtUtil;
+
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, WebFilterChain chain) {
+        String token = exchange.getRequest().getHeaders().getFirst(HEADER_AHTU);
+
+        if (token != null && !token.startsWith("Bearer ") && jwtUtil.checkToken(token)) {
+            return chain.filter(exchange);
+        }
+        throw new RuntimeException();
+    }
+}
