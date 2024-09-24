1711a49f774d0ea8337d0597e7a6254cfcca90c3
junbeom-Son <junbeom247@gmail.com>
2024-09-24 17:06:35 +0900

    refactor: throw new custom exception rather than default exception on not found

--- backend/user/src/main/java/io/ssafy/p/j11a307/user/service/UserService.java
+++ backend/user/src/main/java/io/ssafy/p/j11a307/user/service/UserService.java
@@ -2,6 +2,8 @@ package io.ssafy.p.j11a307.user.service;
 
 import io.ssafy.p.j11a307.user.entity.LeftUser;
 import io.ssafy.p.j11a307.user.entity.User;
+import io.ssafy.p.j11a307.user.exception.BusinessException;
+import io.ssafy.p.j11a307.user.exception.ErrorCode;
 import io.ssafy.p.j11a307.user.repository.LeftUserRepository;
 import io.ssafy.p.j11a307.user.repository.UserRepository;
 import io.ssafy.p.j11a307.user.util.JwtUtil;
@@ -24,7 +26,7 @@ public class UserService {
 
     @Transactional
     public void withdraw(Integer userId) {
-        User user = userRepository.findById(userId).orElseThrow();
+        User user = userRepository.findById(userId).orElseThrow(() -> new BusinessException(ErrorCode.USER_NOT_FOUND));
         LeftUser leftUser = new LeftUser(user);
         userRepository.delete(user);
         leftUserRepository.save(leftUser);
@@ -32,13 +34,13 @@ public class UserService {
 
     @Transactional
     public void toggleOrderStatusAlert(Integer userId, boolean alertOn) {
-        User user = userRepository.findById(userId).orElseThrow();
+        User user = userRepository.findById(userId).orElseThrow(() -> new BusinessException(ErrorCode.USER_NOT_FOUND));
         user.toggleOrderStatusAlert(alertOn);
     }
 
     @Transactional
     public void toggleDibsStoreAlert(Integer userId, boolean alertOn) {
-        User user = userRepository.findById(userId).orElseThrow();
+        User user = userRepository.findById(userId).orElseThrow(() -> new BusinessException(ErrorCode.USER_NOT_FOUND));
         user.toggleDibsStoreAlert(alertOn);
     }
 }
