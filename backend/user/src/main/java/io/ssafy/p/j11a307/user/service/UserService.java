cf8a68d619568e0796e11122a36d8c974d889c08
junbeom-Son <junbeom247@gmail.com>
2024-09-24 14:10:35 +0900

    feat: withdraw service

--- backend/user/src/main/java/io/ssafy/p/j11a307/user/service/UserService.java
+++ backend/user/src/main/java/io/ssafy/p/j11a307/user/service/UserService.java
@@ -1,19 +1,32 @@
 package io.ssafy.p.j11a307.user.service;
 
+import io.ssafy.p.j11a307.user.entity.LeftUser;
+import io.ssafy.p.j11a307.user.entity.User;
+import io.ssafy.p.j11a307.user.repository.LeftUserRepository;
 import io.ssafy.p.j11a307.user.repository.UserRepository;
 import io.ssafy.p.j11a307.user.util.JwtUtil;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
 public class UserService {
 
     private final UserRepository userRepository;
+    private final LeftUserRepository leftUserRepository;
 
     private final JwtUtil jwtUtil;
 
     public Integer getUserId(String accessToken) {
         return jwtUtil.getUserIdFromAccessToken(accessToken);
+    }
+
+    @Transactional
+    public void withdraw(Integer userId) {
+        User user = userRepository.findById(userId).orElseThrow();
+        LeftUser leftUser = new LeftUser(user);
+        userRepository.delete(user);
+        leftUserRepository.save(leftUser);
     }
 }
