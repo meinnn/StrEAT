7650d4aaa2641858800fbcd5ec5ddc3746fed151
yukyung <kyg0954@naver.com>
2024-09-25 06:33:17 +0900

    feat: 사장님 한마디 수정 UI 제작

---
+++ front/src/app/owner/store/setting/announcement/page.tsx
@@ -0,0 +1,9 @@
+import OwnerStoreSettingAccouncement from '@/containers/owner/store/setting/announcement'
+
+export default function OwnerSettingAnnouncementPage() {
+  return (
+    <div>
+      <OwnerStoreSettingAccouncement />
+    </div>
+  )
+}
