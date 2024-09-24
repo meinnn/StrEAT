78e5aa2fcd5caad7759007e4769ed77d4e267c6d
yukyung <kyg0954@naver.com>
2024-09-24 10:50:35 +0900

    fix: owner layout 적용시켜서 tablist 추가

--- front/src/containers/owner/store/index.tsx
+++ front/src/containers/owner/store/index.tsx
@@ -1,10 +1,10 @@
 'use client'
 
-import OwnerTabList from '@/components/OwnerTabList'
 import Image from 'next/image'
 import StoreBusinessLocation from '@/containers/owner/store/StoreBusinessLocation'
 import StoreBusinessHours from '@/containers/owner/store/StoreBusinessHours'
 import StoreInformation from '@/containers/owner/store/StoreInformation'
+import OwnerLayout from '@/components/OwnerLayout'
 
 const TAB_LIST = [
   { name: '내 점포 정보', href: '/store' },
@@ -14,20 +14,21 @@ const TAB_LIST = [
 export default function OwnerStore() {
   return (
     <div className="bg-white">
-      <OwnerTabList tabList={TAB_LIST} />
-      <main className="pb-8 flex flex-col">
-        <div className="relative w-full h-40 overflow-hidden bg-gray-medium">
-          <Image
-            src="/images/보쌈사진.jpg"
-            alt="store"
-            fill
-            className="object-cover"
-          />
-        </div>
-        <StoreInformation />
-        <StoreBusinessLocation />
-        <StoreBusinessHours />
-      </main>
+      <OwnerLayout tabList={TAB_LIST}>
+        <main className="pb-8 flex flex-col">
+          <div className="relative w-full h-40 overflow-hidden bg-gray-medium">
+            <Image
+              src="/images/보쌈사진.jpg"
+              alt="store"
+              fill
+              className="object-cover"
+            />
+          </div>
+          <StoreInformation />
+          <StoreBusinessLocation />
+          <StoreBusinessHours />
+        </main>
+      </OwnerLayout>
     </div>
   )
 }
