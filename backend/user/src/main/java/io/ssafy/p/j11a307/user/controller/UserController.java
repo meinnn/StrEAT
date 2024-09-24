837323b48b1ef57d119798700f79efe398188c03
junbeom-Son <junbeom247@gmail.com>
2024-09-24 14:10:35 +0900

    feat: withdraw service

--- backend/user/src/main/java/io/ssafy/p/j11a307/user/controller/UserController.java
+++ backend/user/src/main/java/io/ssafy/p/j11a307/user/controller/UserController.java
@@ -1,8 +1,12 @@
 package io.ssafy.p.j11a307.user.controller;
 
 import io.ssafy.p.j11a307.user.service.UserService;
+import io.ssafy.p.j11a307.user.util.JwtUtil;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,9 +16,12 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserController {
 
     @Value("{streat.internal-request}")
-    String internalRequestKey;
+    private String internalRequestKey;
+    private String HEADER_AUTH = "Authorization";
 
     private final UserService userService;
+
+    private final JwtUtil jwtUtil;
 
     @GetMapping("/user-id")
     public Integer getUserId(String accessToken, @RequestHeader(value = "X-Internal-Request") String internalRequest) {
@@ -22,5 +29,13 @@ public class UserController {
             return userService.getUserId(accessToken);
         }
         return null;
+    }
+
+    @DeleteMapping("/withdraw")
+    public ResponseEntity<Void> withdraw(HttpServletRequest request) {
+        String accessToken = request.getHeader(HEADER_AUTH);
+        Integer userId = jwtUtil.getUserIdFromAccessToken(accessToken);
+
+        return ResponseEntity.noContent().build();
     }
 }
