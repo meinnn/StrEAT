ba09aa466ccc458b8cd8d594700a0bdf60a9af10
yukyung <kyg0954@naver.com>
2024-09-24 10:08:30 +0900

    feat: 내 점포 정보 페이지 제작

---
+++ front/src/containers/owner/store/index.tsx
@@ -0,0 +1,33 @@
+'use client'
+
+import OwnerTabList from '@/components/OwnerTabList'
+import Image from 'next/image'
+import StoreBusinessLocation from './StoreBusinessLocation'
+import StoreBusinessHours from './StoreBusinessHours'
+import StoreInformation from './StoreInformation'
+
+const TAB_LIST = [
+  { name: '내 점포 정보', href: '/store' },
+  { name: '메뉴 정보', href: '/menu' },
+]
+
+export default function OwnerStore() {
+  return (
+    <div className="bg-white">
+      <OwnerTabList tabList={TAB_LIST} />
+      <main className="pb-8 flex flex-col">
+        <div className="relative w-full h-40 overflow-hidden bg-gray-medium">
+          <Image
+            src="/images/보쌈사진.jpg"
+            alt="store"
+            fill
+            className="object-cover"
+          />
+        </div>
+        <StoreInformation />
+        <StoreBusinessLocation />
+        <StoreBusinessHours />
+      </main>
+    </div>
+  )
+}
