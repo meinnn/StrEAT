611b0d5f6ece3f23b2faf931bf7123a2efc9e696
junbeom-Son <junbeom247@gmail.com>
2024-09-24 13:56:19 +0900

    feat: check whether the request is made from internal service

--- backend/user/src/main/java/io/ssafy/p/j11a307/user/controller/UserController.java
+++ backend/user/src/main/java/io/ssafy/p/j11a307/user/controller/UserController.java
@@ -2,17 +2,25 @@ package io.ssafy.p.j11a307.user.controller;
 
 import io.ssafy.p.j11a307.user.service.UserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
 public class UserController {
 
+    @Value("{streat.internal-request}")
+    String internalRequestKey;
+
     private final UserService userService;
 
     @GetMapping("/user-id")
-    public Integer getUserId(String accessToken) {
-        return userService.getUserId(accessToken);
+    public Integer getUserId(String accessToken, @RequestHeader(value = "X-Internal-Request") String internalRequest) {
+        if (internalRequestKey.equals(internalRequest)) {
+            return userService.getUserId(accessToken);
+        }
+        return null;
     }
 }
