ca81424e5200adb95f7657073d24d4562a4580cb
junbeom-Son <junbeom247@gmail.com>
2024-09-26 12:56:58 +0900

    feat: create push alert micro service

---
+++ backend/push-alert/src/main/java/io/ssafy/p/j11a307/push_alert/PushAlertApplication.java
@@ -0,0 +1,13 @@
+package io.ssafy.p.j11a307.push_alert;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PushAlertApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PushAlertApplication.class, args);
+	}
+
+}
