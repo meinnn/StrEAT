55bf5ab1cd60984aafce5ef1cd66469f6b7f7ed5
junbeom-Son <junbeom247@gmail.com>
2024-09-27 15:17:40 +0900

    refactor: modify to receive multiple request at one method to reuse

--- backend/push-alert/src/main/java/io/ssafy/p/j11a307/push_alert/controller/AlertController.java
+++ backend/push-alert/src/main/java/io/ssafy/p/j11a307/push_alert/controller/AlertController.java
@@ -4,6 +4,7 @@ import io.ssafy.p.j11a307.push_alert.dto.alerts.AlertType;
 import io.ssafy.p.j11a307.push_alert.exception.BusinessException;
 import io.ssafy.p.j11a307.push_alert.exception.ErrorCode;
 import io.ssafy.p.j11a307.push_alert.service.AlertService;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -19,12 +20,17 @@ public class AlertController {
 
     private final AlertService alertService;
 
-    @GetMapping("/order-accept")
-    public void sendOrderAcceptAlert(Integer customerId, Integer orderId, String storeName, @RequestHeader(value = "X-Internal-Request") String internalRequest) {
+    @GetMapping(value = {"/order-accept"})
+    public void sendOrderStatusChangeAlert(
+            Integer customerId, Integer orderId, String storeName,
+            @RequestHeader(value = "X-Internal-Request") String internalRequest,
+            HttpServletRequest request) {
         if (!internalRequestKey.equals(internalRequest)) {
             // MSA 내부 접속이 아닌 경우 에러 발생
             throw new BusinessException(ErrorCode.BAD_REQUEST);
         }
-        alertService.sendOrderStatusChangeAlert(customerId, orderId, storeName, AlertType.ORDER_ACCEPTED);
+        String[] requestUri = request.getRequestURI().split("/");
+        AlertType alertType = AlertType.getByRequestUri(requestUri[requestUri.length - 1]);
+        alertService.sendOrderStatusChangeAlert(customerId, orderId, storeName, alertType);
     }
 }
--- backend/push-alert/src/main/java/io/ssafy/p/j11a307/push_alert/dto/alerts/AlertType.java
+++ backend/push-alert/src/main/java/io/ssafy/p/j11a307/push_alert/dto/alerts/AlertType.java
@@ -1,14 +1,27 @@
 package io.ssafy.p.j11a307.push_alert.dto.alerts;
 
+import io.ssafy.p.j11a307.push_alert.exception.BusinessException;
+import io.ssafy.p.j11a307.push_alert.exception.ErrorCode;
 import lombok.Getter;
+
+import java.util.Arrays;
 
 @Getter
 public enum AlertType {
-    ORDER_ACCEPTED("주문 수락! 맛있게 만들고 있어요");
+    ORDER_ACCEPTED("주문 수락! 맛있게 만들고 있어요", "order-accept");
 
     private final String message;
+    private final String requestUri;
 
-    AlertType(String message) {
+    AlertType(String message, String requestUri) {
         this.message = message;
+        this.requestUri = requestUri;
+    }
+
+    public static AlertType getByRequestUri(String requestUri) {
+        return Arrays.stream(AlertType.values())
+                .filter(alertType -> alertType.requestUri.equals(requestUri))
+                .findFirst()
+                .orElseThrow(() -> new BusinessException(ErrorCode.ALERT_TYPE_NOT_FOUND));
     }
 }
