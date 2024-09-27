5c8e56ae798f532c253f6020584b23fc40bbc0c4
junbeom-Son <junbeom247@gmail.com>
2024-09-27 10:02:44 +0900

    chore: remove backend file

--- backend/store/src/main/java/io/ssafy/p/j11a307/store/repository/StoreRepository.java
+++
@@ -1,8 +0,0 @@
-package io.ssafy.p.j11a307.store.repository;
-
-import io.ssafy.p.j11a307.store.entity.Store;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface StoreRepository extends JpaRepository<Store, Long> {
-
-}
--- backend/user/src/main/java/io/ssafy/p/j11a307/user/repository/LeftUserRepository.java
+++
@@ -1,7 +0,0 @@
-package io.ssafy.p.j11a307.user.repository;
-
-import io.ssafy.p.j11a307.user.entity.LeftUser;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface LeftUserRepository extends JpaRepository<LeftUser, Integer> {
-}
