3627ae9a3528697b951a880e56248099f63f55db
junbeom-Son <junbeom247@gmail.com>
2024-09-27 14:30:32 +0900

    feat: create fcm order status change alert

--- backend/push-alert/src/main/java/io/ssafy/p/j11a307/push_alert/dto/alerts/AlertType.java
+++ backend/push-alert/src/main/java/io/ssafy/p/j11a307/push_alert/dto/alerts/AlertType.java
@@ -1,4 +1,14 @@
 package io.ssafy.p.j11a307.push_alert.dto.alerts;
 
+import lombok.Getter;
+
+@Getter
 public enum AlertType {
+    ORDER_ACCEPTED("주문 수락! 맛있게 만들고 있어요");
+
+    private final String message;
+
+    AlertType(String message) {
+        this.message = message;
+    }
 }
