30daad18ae4a95f7e2aa64d53fea9d1df2119433
junbeom-Son <junbeom247@gmail.com>
2024-09-27 14:33:20 +0900

    feat: send order acceptance push alert

--- backend/push-alert/src/main/java/io/ssafy/p/j11a307/push_alert/controller/AlertController.java
+++ backend/push-alert/src/main/java/io/ssafy/p/j11a307/push_alert/controller/AlertController.java
@@ -1,12 +1,30 @@
 package io.ssafy.p.j11a307.push_alert.controller;
 
+import io.ssafy.p.j11a307.push_alert.dto.alerts.AlertType;
+import io.ssafy.p.j11a307.push_alert.exception.BusinessException;
+import io.ssafy.p.j11a307.push_alert.exception.ErrorCode;
 import io.ssafy.p.j11a307.push_alert.service.AlertService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
 @RestController
 public class AlertController {
 
+    @Value("{streat.internal-request}")
+    private String internalRequestKey;
+
     private final AlertService alertService;
+
+    @GetMapping("/order-reject")
+    public void sendOrderRejectAlert(Integer customerId, Integer orderId, String storeName, @RequestHeader(value = "X-Internal-Request") String internalRequest) {
+        if (!internalRequestKey.equals(internalRequest)) {
+            // MSA 내부 접속이 아닌 경우 에러 발생
+            throw new BusinessException(ErrorCode.BAD_REQUEST);
+        }
+        alertService.sendOrderStatusChangeAlert(customerId, orderId, storeName, AlertType.ORDER_ACCEPTED);
+    }
 }
--- backend/push-alert/src/main/java/io/ssafy/p/j11a307/push_alert/service/AlertService.java
+++ backend/push-alert/src/main/java/io/ssafy/p/j11a307/push_alert/service/AlertService.java
@@ -1,7 +1,41 @@
 package io.ssafy.p.j11a307.push_alert.service;
 
+import io.ssafy.p.j11a307.push_alert.dto.FcmNotification;
+import io.ssafy.p.j11a307.push_alert.dto.alerts.AlertType;
+import io.ssafy.p.j11a307.push_alert.dto.alerts.FcmAlertData;
+import io.ssafy.p.j11a307.push_alert.dto.alerts.FcmOrderStatusChangeAlert;
+import io.ssafy.p.j11a307.push_alert.util.FirebaseUtil;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 @Service
+@RequiredArgsConstructor
 public class AlertService {
+
+    @Value("{streat.internal-request}")
+    private String internalRequestKey;
+
+    private final UserService userService;
+
+    private final FirebaseUtil firebaseUtil;
+
+    public void sendOrderStatusChangeAlert(Integer customerId, Integer orderId, String storeName, AlertType alertType) {
+        String customerFcmToken = userService.getFcmTokenByUserId(customerId, internalRequestKey);
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy년 MM월 dd일 HH:mm");
+        FcmAlertData data = FcmOrderStatusChangeAlert.builder()
+                .orderId(String.valueOf(orderId))
+                .storeName(storeName)
+                .createdAt(simpleDateFormat.format(new Date()))
+                .alertType(alertType)
+                .build();
+        FcmNotification notification = FcmNotification.builder()
+                .body(data.getMessage())
+                .build();
+
+        firebaseUtil.pushAlert(data, customerFcmToken, notification);
+    }
 }
