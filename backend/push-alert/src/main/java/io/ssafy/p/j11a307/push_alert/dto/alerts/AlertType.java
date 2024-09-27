0d29a58ab232a8e75559ffb434648e4af7805afd
junbeom-Son <junbeom247@gmail.com>
2024-09-27 13:22:36 +0900

    feat: create fcm relative classes

---
+++ backend/push-alert/src/main/java/io/ssafy/p/j11a307/push_alert/dto/alerts/AlertType.java
@@ -0,0 +1,4 @@
+package io.ssafy.p.j11a307.push_alert.dto.alerts;
+
+public enum AlertType {
+}
---
+++ backend/push-alert/src/main/java/io/ssafy/p/j11a307/push_alert/dto/alerts/FcmAlertData.java
@@ -0,0 +1,4 @@
+package io.ssafy.p.j11a307.push_alert.dto.alerts;
+
+public interface FcmAlertData {
+}
