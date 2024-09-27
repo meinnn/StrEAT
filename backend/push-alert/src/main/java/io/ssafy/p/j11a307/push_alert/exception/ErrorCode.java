3a370dc90d36bc611415edc4b2c2ea74ef9f657c
junbeom-Son <junbeom247@gmail.com>
2024-09-27 12:18:54 +0900

    feat: create custom exception

---
+++ backend/push-alert/src/main/java/io/ssafy/p/j11a307/push_alert/exception/ErrorCode.java
@@ -0,0 +1,18 @@
+package io.ssafy.p.j11a307.push_alert.exception;
+
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+public enum ErrorCode {
+
+    BAD_REQUEST(HttpStatus.BAD_REQUEST, "잘못된 접근입니다.");
+
+    private final HttpStatus status;
+    private final String message;
+
+    ErrorCode(HttpStatus status, String message) {
+        this.status = status;
+        this.message = message;
+    }
+}
