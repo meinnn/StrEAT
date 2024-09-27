bc8b17c899469f95c7802d75722ae16d11be6de2
junbeom-Son <junbeom247@gmail.com>
2024-09-27 15:19:16 +0900

    feat: push order requested alert

--- backend/push-alert/src/main/java/io/ssafy/p/j11a307/push_alert/dto/alerts/AlertType.java
+++ backend/push-alert/src/main/java/io/ssafy/p/j11a307/push_alert/dto/alerts/AlertType.java
@@ -8,7 +8,8 @@ import java.util.Arrays;
 
 @Getter
 public enum AlertType {
-    ORDER_ACCEPTED("주문 수락! 맛있게 만들고 있어요", "order-accept");
+    ORDER_ACCEPTED("주문 수락! 맛있게 만들고 있어요", "order-accept"),
+    ORDER_REQUESTED("주문 요청 완료! 사장님 수락 후 조리가 시작돼요", "order-requested"),;
 
     private final String message;
     private final String requestUri;
