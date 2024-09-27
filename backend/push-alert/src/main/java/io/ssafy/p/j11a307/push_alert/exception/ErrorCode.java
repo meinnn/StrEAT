3fbb6263481d680bcc100ed278e26b18f1c333aa
junbeom-Son <junbeom247@gmail.com>
2024-09-27 14:47:53 +0900

    feat: add new exception

--- backend/push-alert/src/main/java/io/ssafy/p/j11a307/push_alert/exception/ErrorCode.java
+++ backend/push-alert/src/main/java/io/ssafy/p/j11a307/push_alert/exception/ErrorCode.java
@@ -7,7 +7,8 @@ import org.springframework.http.HttpStatus;
 public enum ErrorCode {
 
     BAD_REQUEST(HttpStatus.BAD_REQUEST, "잘못된 접근입니다."),
-    PUSH_ALERT_FAILED(HttpStatus.INTERNAL_SERVER_ERROR, "푸시 알림 전송에 실패했습니다.");
+    PUSH_ALERT_FAILED(HttpStatus.INTERNAL_SERVER_ERROR, "푸시 알림 전송에 실패했습니다."),
+    ALERT_TYPE_NOT_FOUND(HttpStatus.NOT_FOUND, "푸시 알림 정보를 찾지 못했습니다.");
 
     private final HttpStatus status;
     private final String message;
