0482808c702be879a8ee3a9e2ce1d320b53a8d4e
dana019 <dana019@naver.com>
2024-09-30 17:14:52 +0900

    fix: 환경변수 NEXT_PUBLIC 설정

--- front/src/app/api/login/kakao/route.ts
+++ front/src/app/api/login/kakao/route.ts
@@ -2,8 +2,8 @@ import { NextResponse } from 'next/server'
 
 // eslint-disable-next-line import/prefer-default-export
 export async function GET() {
-  const clientId = process.env.KAKAO_APP_KEY // 환경 변수에서 REST API 키 가져오기
-  const redirectUri = process.env.KAKAO_REDIRECT_URI // 환경 변수에서 리다이렉트 URI 가져오기
+  const clientId = process.env.NEXT_PUBLIC_KAKAO_APP_KEY // 환경 변수에서 REST API 키 가져오기
+  const redirectUri = process.env.NEXT_PUBLIC_KAKAO_REDIRECT_URI // 환경 변수에서 리다이렉트 URI 가져오기
   const responseType = 'code'
 
   // 카카오 로그인 URL 생성
