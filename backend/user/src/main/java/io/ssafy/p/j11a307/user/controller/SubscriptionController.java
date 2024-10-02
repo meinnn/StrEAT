3145980bc82081ce93027b0f1be23c285ccbb73a
junbeom-Son <junbeom247@gmail.com>
2024-10-02 17:28:46 +0900

    feat: subscript store

---
+++ backend/user/src/main/java/io/ssafy/p/j11a307/user/controller/SubscriptionController.java
@@ -0,0 +1,36 @@
+package io.ssafy.p.j11a307.user.controller;
+
+import io.ssafy.p.j11a307.user.service.SubscriptionService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/subscriptions")
+@RequiredArgsConstructor
+public class SubscriptionController {
+
+    private final String HEADER_AUTH = "Authorization";
+
+    private final SubscriptionService subscriptionService;
+
+    @PostMapping("/{storeId}")
+    @Operation(summary = "가게 구독", description = "가게 구독")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "가게 구독 성공")
+    })
+    public ResponseEntity<Void> subscriptStore(@PathVariable Integer storeId, HttpServletRequest request) {
+        String accessToken = request.getHeader(HEADER_AUTH);
+        subscriptionService.subscript(accessToken, storeId);
+        return new ResponseEntity<>(HttpStatus.CREATED);
+    }
+}
