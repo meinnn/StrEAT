7e7b8e8b475ae255354a856e6fa19d83fbbed2f6
junbeom-Son <junbeom247@gmail.com>
2024-10-02 14:34:13 +0900

    feat: create jpa repository

--- backend/payment/src/main/java/io/ssafy/p/j11a307/payment/repository/PaymentRepository.java
+++ backend/payment/src/main/java/io/ssafy/p/j11a307/payment/repository/PaymentRepository.java
@@ -1,6 +1,7 @@
 package io.ssafy.p.j11a307.payment.repository;
 
+import io.ssafy.p.j11a307.payment.entity.Payment;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface PaymentRepository {
+public interface PaymentRepository extends JpaRepository<Payment, Integer> {
 }
