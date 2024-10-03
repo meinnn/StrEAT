6650929020290609f211dfdd34bb392a5487c17f
yukyung <kyg0954@naver.com>
2024-10-04 01:56:09 +0900

    feat: 내 리뷰 삭제 api 연결

--- front/src/containers/customer/mypage/components/ReviewListItem/index.tsx
+++ front/src/containers/customer/mypage/components/ReviewListItem/index.tsx
@@ -1,9 +1,31 @@
-import Image from 'next/image'
-import Link from 'next/link'
+'use client'
+
+import { useMutation, useQueryClient } from '@tanstack/react-query'
 import { FaStar } from 'react-icons/fa'
 import { GoChevronRight } from 'react-icons/go'
+import Link from 'next/link'
+import Image from 'next/image'
 import { TbReceipt } from 'react-icons/tb'
+import { useState } from 'react'
 import ImageCrousel from '@/components/ImageCarousel'
+import DeleteConfirmationModal from '@/containers/customer/mypage/DeleteConfirmationModal'
+
+const deleteReview = async (reviewId: number) => {
+  const response = await fetch(`/api/review/${reviewId}`, {
+    method: 'DELETE',
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization':
+        'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJhY2Nlc3MtdG9rZW4iLCJpYXQiOjE3Mjc4MzE0MTQsImV4cCI6MjA4NzgzMTQxNCwidXNlcklkIjoxMn0.UrVrI-WUCXdx017R4uRIl6lzxbktVSfEDjEgYe5J8UQ',
+    },
+  })
+
+  if (!response.ok) {
+    throw new Error('리뷰 삭제에 실패했습니다.')
+  }
+
+  return response.json()
+}
 
 export default function ReviewListItem({
   reviewId,
@@ -26,8 +48,42 @@ export default function ReviewListItem({
   score: number
   reviewImageList: string[]
 }) {
+  const queryClient = useQueryClient()
+
+  const mutation = useMutation({
+    mutationFn: () => deleteReview(reviewId),
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ['/api/review/me'] })
+    },
+    onError: (error: Error) => {
+      console.error('Error:', error)
+      console.error('리뷰 삭제 중 오류가 발생했습니다.')
+    },
+  })
+
+  const [isModalOpen, setIsModalOpen] = useState(false)
+
+  const handleClickDeleteBtn = () => {
+    mutation.mutate()
+    setIsModalOpen(true)
+  }
+
+  const handleConfirmDelete = () => {
+    setIsModalOpen(false)
+  }
+
+  const handleCancelDelete = () => {
+    setIsModalOpen(false)
+  }
+
   return (
     <section className=" px-5 pb-5 border-b border-gray-medium last:border-b-0 bg-white">
+      <DeleteConfirmationModal
+        isOpen={isModalOpen}
+        onConfirm={handleConfirmDelete}
+        onCancel={handleCancelDelete}
+      />
+
       <div className="flex py-3 items-center gap-3 relative">
         <p className="relative shrink-0 w-10 aspect-square rounded-md overflow-hidden border border-gray-medium bg-gray-light">
           <Image
@@ -48,24 +104,25 @@ export default function ReviewListItem({
             </Link>
           </div>
           <div className="flex text-xs text-gray-dark">
-            {orderList.slice(0, 1).map((order) => {
-              return (
-                <p
-                  key={order}
-                  className="text-gray-dark text-[0.625rem] font-normal"
-                >
-                  {order}
-                </p>
-              )
-            })}
-            {orderList.length > 1 ? (
+            {orderList.slice(0, 1).map((order) => (
+              <p
+                key={order}
+                className="text-gray-dark text-[0.625rem] font-normal"
+              >
+                {order}
+              </p>
+            ))}
+            {orderList.length > 1 && (
               <p className="text-gray-dark text-[0.625rem] font-normal">
                 &nbsp;외 {orderList.length - 1}개
               </p>
-            ) : null}
+            )}
           </div>
         </div>
-        <button className="absolute right-1 top-5 bg-white text-gray-dark border border-gray-dark px-3 rounded-full text-sm">
+        <button
+          onClick={handleClickDeleteBtn}
+          className="absolute right-1 top-5 bg-white text-gray-dark border border-gray-dark px-3 rounded-full text-sm"
+        >
           삭제
         </button>
       </div>
@@ -74,32 +131,29 @@ export default function ReviewListItem({
           {[1, 2, 3, 4, 5].map((star) => (
             <FaStar
               key={star}
-              className={`h-5 w-5 ${score >= star ? 'text-[#FBC400]' : 'text-gray-medium'}`}
+              className={`h-5 w-5 ${
+                score >= star ? 'text-[#FBC400]' : 'text-gray-medium'
+              }`}
             />
           ))}
         </div>
         <p className="text-xs">{date}</p>
       </div>
-      {reviewImageList.length > 0 ? (
-        <ImageCrousel slides={reviewImageList} />
-      ) : null}
-
+      {reviewImageList.length > 0 && <ImageCrousel slides={reviewImageList} />}
       <div className="mb-4 mt-2">
         <p className="font-normal text-text">{content}</p>
       </div>
       <div className="flex gap-1 items-start">
         <TbReceipt className="w-6 h-6 flex-shrink-0 mt-1 text-primary-500" />
         <div className="flex flex-wrap gap-2">
-          {orderList.map((order) => {
-            return (
-              <span
-                key={order}
-                className="rounded-full text-sm border py-1 px-3 border-primary-500 text-primary-500"
-              >
-                {order}
-              </span>
-            )
-          })}
+          {orderList.map((order) => (
+            <span
+              key={order}
+              className="rounded-full text-sm border py-1 px-3 border-primary-500 text-primary-500"
+            >
+              {order}
+            </span>
+          ))}
         </div>
       </div>
     </section>
--- front/tailwind.config.ts
+++ front/tailwind.config.ts
@@ -42,6 +42,10 @@ const config: Config = {
           'linear-gradient(180deg, #FFFBF3 0%, #FFF 28.5%)',
       },
       keyframes: {
+        'short-slide-up': {
+          '0%': { transform: 'translateY(0%)', opacity: '0.5' },
+          '100%': { transform: 'translateY(-20%)', opacity: '1' },
+        },
         'slide-up': {
           '0%': { transform: 'translateY(100%)' },
           '100%': { transform: 'translateY(0)' },
@@ -54,6 +58,7 @@ const config: Config = {
       animation: {
         'slide-up': 'slide-up 0.2s ease-out forwards',
         'slide-down': 'slide-down 0.2s ease-out forwards',
+        'modal-short-slide-up': 'short-slide-up 0.2s ease-out forwards',
       },
       boxShadow: {
         'inner-strong': 'inset 10px 10px 20px rgba(255, 236, 199, 0.3)',
