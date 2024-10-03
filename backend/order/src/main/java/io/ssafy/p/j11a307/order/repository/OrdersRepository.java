801062d824dd9f9fde6482de3097a73fdccca669
이주호 <jeuslee99@naver.com>
2024-10-04 00:42:35 +0900

    fix: 페이징 에러 수정

--- backend/order/src/main/java/io/ssafy/p/j11a307/order/repository/OrdersRepository.java
+++ backend/order/src/main/java/io/ssafy/p/j11a307/order/repository/OrdersRepository.java
@@ -4,6 +4,7 @@ import io.ssafy.p.j11a307.order.entity.Orders;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -14,4 +15,11 @@ public interface OrdersRepository extends JpaRepository<Orders, Integer> {
     Page<Orders> findByUserId(Integer userId, Pageable pageable);
 
     Page<Orders> findByStoreId(Integer storeId, Pageable pageable);
+
+    @Query(value = "SELECT * FROM orders o WHERE o.user_id = :userId AND EXISTS (SELECT * FROM review r WHERE r.review_id = o.id)", nativeQuery = true)
+    Page<Orders> findByUserIdAndHasReview(Integer userId, Pageable pageable);
+
+    @Query(value = "SELECT * FROM orders o WHERE o.store_id = :storeId AND EXISTS (SELECT * FROM review r WHERE r.review_id = o.id)", nativeQuery = true)
+    Page<Orders> findByStoreIdAndHasReview(Integer storeId, Pageable pageable);
+
 }
