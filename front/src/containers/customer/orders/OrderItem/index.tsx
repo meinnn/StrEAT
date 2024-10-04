dd12f836aba530348a6f015c92c90747457a96d6
yukyung <kyg0954@naver.com>
2024-10-04 12:08:27 +0900

    feat: 내 주문내역 조회 api 연결

--- front/src/containers/customer/orders/OrderItem/index.tsx
+++ front/src/containers/customer/orders/OrderItem/index.tsx
@@ -5,44 +5,104 @@ import Link from 'next/link'
 import { useRouter } from 'next/navigation'
 import { GoChevronRight } from 'react-icons/go'
 
+const PROCESS_ID = {
+  REJECTED: 0,
+  WAITING_FOR_PROCESSING: 1,
+  PROCESSING: 2,
+  WAITING_FOR_RECEIPT: 3,
+  RECEIVED: 4,
+}
+
+const PROCESS = [
+  {
+    id: 'REJECTED',
+    orderNum: '0',
+    label: '거절',
+  },
+  {
+    id: 'WAITING_FOR_PROCESSING',
+    orderNum: '1',
+    label: '주문 요청',
+  },
+  {
+    id: 'PROCESSING',
+    orderNum: '2',
+    label: '주문 확인',
+  },
+  {
+    id: 'WAITING_FOR_RECEIPT',
+    orderNum: '3',
+    label: '픽업 대기',
+  },
+  {
+    id: 'RECEIVED',
+    orderNum: '4',
+    label: '픽업 완료',
+  },
+]
+
+type ProcessIdKeys = keyof typeof PROCESS_ID
+
 export default function OrderItem({
   id,
   date,
-  imageUrl,
-  title,
+  storeImageUrl,
+  storeName,
   orderList,
-  progress,
-  review,
+  orderStatus,
+  isReviewed,
 }: {
   id: number
   date: string
-  imageUrl: string
-  title: string
-  orderList: string[]
-  progress: number
-  review: boolean
+  storeImageUrl: string
+  storeName: string
+  orderList: { productName: string; orderProductCount: number }[]
+  orderStatus: ProcessIdKeys
+  isReviewed: boolean
 }) {
   const router = useRouter()
 
   return (
-    <section className="pt-3 pb-2 flex flex-col  shadow-md bg-white rounded-lg overflow-hidden">
-      <div className="px-6">
-        <div className="flex gap-2 items-center pb-3 justify-between">
-          <p className="text-xs  text-text">{date}</p>
+    <section className="pt-3 pb-2 flex flex-col relative shadow-md bg-white rounded-lg overflow-hidden">
+      {PROCESS_ID[orderStatus] === 0 ? (
+        <div className="absolute top-0 left-0 rounded-lg flex flex-col justify-center items-center w-full bg-[rgba(255,236,199,0.8)] h-full z-10">
+          <Image
+            src="/images/no_content_illustration.png"
+            className="object-cover"
+            alt="내용이 없다는 일러스트"
+            width={60}
+            height={60}
+          />
+          <p className="text-text">주문 거절</p>
           <Link href={`/customer/orders/${id}`}>
             <button
               type="button"
-              className="text-xs border border-primary-500 py-0 px-2 h-6 rounded-full text-primary-500"
+              className="absolute top-3 right-6 text-xs py-0 px-2 h-6 rounded-full bg-text text-secondary-light"
             >
               주문 상세
             </button>
           </Link>
         </div>
+      ) : null}
+      <div className="px-6">
+        <div className="flex gap-2 items-center pb-3 justify-between">
+          <p className="text-xs text-text">{date}</p>
+          {PROCESS_ID[orderStatus] !== 0 ? (
+            <Link href={`/customer/orders/${id}`}>
+              <button
+                type="button"
+                className="text-xs border border-primary-500 py-0 px-2 h-6 rounded-full text-primary-500"
+              >
+                주문 상세
+              </button>
+            </Link>
+          ) : null}
+        </div>
         <div className="flex justify-between w-full items-center pb-4">
           <div className="flex items-center gap-3">
             <p className="relative flex-shrink-0 w-16 h-16 aspect-square rounded-lg overflow-hidden bg-gray-medium">
               <Image
-                src={imageUrl}
+                src={storeImageUrl || '/images/보쌈사진.jpg'}
                 alt="가게 사진"
                 fill
                 className="object-cover"
@@ -55,17 +115,17 @@ export default function OrderItem({
                   href={`/customer/stores/${id}`}
                   className="flex items-center gap-2 text-text"
                 >
-                  <h3 className="font-normal">{title}</h3>
+                  <h3 className="font-normal">{storeName}</h3>
                   <GoChevronRight className="w-4 h-4 flex-shrink-0" />
                 </Link>
                 <div className="flex flex-col">
                   {orderList.slice(0, 2).map((order) => {
                     return (
                       <p
-                        key={order}
+                        key={order.productName}
                         className="text-gray-dark text-[0.625rem] font-normal"
                       >
-                        {order}
+                        {order.productName}&nbsp;{order.orderProductCount}개
                       </p>
                     )
                   })}
@@ -78,26 +138,28 @@ export default function OrderItem({
               </div>
             </div>
           </div>
-          {progress !== 4 ? (
-            <div className="flex flex-col gap-2 items-center">
-              <span className="rounded-full w-8 h-8 font-normal bg-primary-500 flex justify-center items-center text-secondary-light">
-                {progress}
-              </span>
-              <p className="text-xs font-semibold text-primary-500">
-                주문 확인
-              </p>
-            </div>
-          ) : null}
+          <div className="flex flex-col gap-2 items-center">
+            {PROCESS_ID[orderStatus] && PROCESS_ID[orderStatus] !== 0 ? (
+              <>
+                <span className="rounded-full w-8 h-8 font-normal bg-primary-500 flex justify-center items-center text-secondary-light">
+                  {PROCESS_ID[orderStatus]}
+                </span>
+                <p className="text-xs font-semibold text-primary-500">
+                  {PROCESS[PROCESS_ID[orderStatus]].label}
+                </p>
+              </>
+            ) : null}
+          </div>
         </div>
       </div>
-      {progress === 4 ? (
+      {orderStatus === 'RECEIVED' ? (
         <div className="w-full px-2">
           <button
             onClick={() => router.push('/customer/orders/1/review/new')}
             type="button"
-            className={`${review ? ' bg-gray-medium text-text opacity-50' : ' bg-primary-500 text-secondary-light'} font-normal py-2 w-full rounded-lg`}
+            className={`${isReviewed ? ' bg-gray-medium text-text opacity-50' : ' bg-primary-500 text-secondary-light'} font-normal py-2 w-full rounded-lg`}
           >
-            {review ? '리뷰 작성완료' : '리뷰 작성하기'}
+            {isReviewed ? '리뷰 작성완료' : '리뷰 작성하기'}
           </button>
         </div>
       ) : null}
--- front/src/containers/customer/orders/index.tsx
+++ front/src/containers/customer/orders/index.tsx
@@ -1,82 +1,156 @@
+'use client'
+
+import { useInfiniteQuery } from '@tanstack/react-query'
+import { useEffect, useRef } from 'react'
 import AppBar from '@/components/AppBar'
 import OrderItem from '@/containers/customer/orders/OrderItem'
+import ReviewSkeleton from '@/components/skeleton/ReviewSkeleton'
+import getConvertedDate from '@/utils/getConvertedDate'
 
-const ORDER_LIST = [
-  {
-    id: 1,
-    date: '2024.09.09 (월)',
-    imageUrl: '/images/보쌈사진.jpg',
-    title: '맛있는 보쌈/족발',
-    orderList: [
-      '매운 족발 小 1개',
-      '보통 족발 中 2개',
-      '매운 족발 大 1개',
-      '매운 족발 大 1개',
-    ],
-    progress: 2,
-    review: false,
-  },
-  {
-    id: 2,
-    date: '2024.09.09 (월)',
-    imageUrl: '/images/보쌈사진.jpg',
-    title: '맛있는 보쌈/족발',
-    orderList: ['매운 족발 小 1개', '보통 족발 中 2개', '매운 족발 大 1개'],
-    progress: 4,
-    review: false,
-  },
-  {
-    id: 3,
-    date: '2024.09.08 (일)',
-    imageUrl: '/images/보쌈사진.jpg',
-    title: '맛있는 보쌈/족발',
-    orderList: ['매운 족발 小 1개', '보통 족발 中 2개', '매운 족발 大 1개'],
-    progress: 4,
-    review: true,
-  },
-  {
-    id: 4,
-    date: '2024.09.08 (일)',
-    imageUrl: '/images/보쌈사진.jpg',
-    title: '맛있는 보쌈/족발',
-    orderList: ['매운 족발 小 1개', '보통 족발 中 2개', '매운 족발 大 1개'],
-    progress: 4,
-    review: true,
-  },
-]
+const PROCESS_ID = {
+  REJECTED: 0,
+  WAITING_FOR_PROCESSING: 1,
+  PROCESSING: 2,
+  WAITING_FOR_RECEIPT: 3,
+  RECEIVED: 4,
+}
+
+type ProcessIdKeys = keyof typeof PROCESS_ID
+
+interface Order {
+  isReviewed: boolean
+  orderStatus: ProcessIdKeys
+  ordersCreatedAt: string
+  ordersId: number
+  products: {
+    productName: string
+    orderProductCount: number
+  }[]
+  storeId: number
+  storeName: string
+  storePhoto: string
+}
+
+interface Page {
+  data: {
+    getMyOrderList: Order[]
+    totalDataCount: number
+    totalPageCount: number
+  }
+  hasMore: boolean
+}
+
+const fetchMyOrderList = async ({ pageParam = 0 }: any) => {
+  try {
+    const response = await fetch(`/api/order/me?page=${pageParam}&limit=5`, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization':
+          'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJhY2Nlc3MtdG9rZW4iLCJpYXQiOjE3Mjc4MzE0MTQsImV4cCI6MjA4NzgzMTQxNCwidXNlcklkIjoxMn0.UrVrI-WUCXdx017R4uRIl6lzxbktVSfEDjEgYe5J8UQ',
+      },
+    })
+
+    if (!response.ok) {
+      throw new Error('Failed to fetch review')
+    }
+
+    return await response.json()
+  } catch (error) {
+    console.error('Error:', error)
+    throw error
+  }
+}
 
 export default function Orders() {
   let lastDate = ''
+
+  const {
+    data: orderListData,
+    fetchNextPage,
+    hasNextPage,
+    isFetchingNextPage,
+    status,
+  } = useInfiniteQuery<Page, Error>({
+    queryFn: async ({ pageParam = 0 }) => fetchMyOrderList({ pageParam }),
+    queryKey: ['/api/review/me'],
+    getNextPageParam: (lastPage: Page, pages: Page[]) => {
+      if (lastPage?.hasMore) {
+        return pages.length
+      }
+      return undefined
+    },
+    initialPageParam: 0,
+  })
+
+  const observerElem = useRef(null)
+
+  useEffect(() => {
+    const currentElem = observerElem.current
+
+    const observer = new IntersectionObserver(
+      (entries) => {
+        if (entries[0].isIntersecting && hasNextPage) {
+          fetchNextPage()
+        }
+      },
+      {
+        rootMargin: '100px',
+      }
+    )
+
+    if (currentElem) observer.observe(currentElem)
+
+    return () => {
+      if (currentElem) observer.unobserve(currentElem)
+    }
+  }, [fetchNextPage, hasNextPage])
+
+  if (status === 'pending') {
+    return <ReviewSkeleton />
+  }
 
   return (
     <div className="bg-secondary-light">
       <AppBar title="주문내역 조회" />
       <main className=" px-4 flex flex-col pb-32">
         <div className="flex flex-col gap-1">
-          {ORDER_LIST.map((order) => {
-            const isVisibleDate = lastDate !== order.date
-            lastDate = order.date
+          {orderListData && orderListData?.pages?.length > 0 ? (
+            <>
+              {orderListData?.pages?.map((page) =>
+                page?.data?.getMyOrderList.map((order) => {
+                  const isVisibleDate =
+                    lastDate !== getConvertedDate(order.ordersCreatedAt)
+                  lastDate = getConvertedDate(order.ordersCreatedAt)
 
-            return (
-              <div key={order.id}>
-                {isVisibleDate && (
-                  <h2 className="font-medium pb-5 pt-7 text-text">
-                    {order.date}
-                  </h2>
-                )}
-                <OrderItem
-                  key={order.id}
-                  id={order.id}
-                  date={order.date}
-                  imageUrl={order.imageUrl}
-                  title={order.title}
-                  orderList={order.orderList}
-                  progress={order.progress}
-                  review={order.review}
-                />
+                  return (
+                    <div key={order.ordersId}>
+                      {isVisibleDate && (
+                        <h2 className="font-medium pb-5 pt-7 text-text">
+                          {getConvertedDate(order.ordersCreatedAt)}
+                        </h2>
+                      )}
+                      <OrderItem
+                        key={order.ordersId}
+                        id={order.ordersId}
+                        date={getConvertedDate(order.ordersCreatedAt)}
+                        storeImageUrl={order.storePhoto}
+                        storeName={order.storeName}
+                        orderList={order.products}
+                        orderStatus={order.orderStatus}
+                        isReviewed={order.isReviewed}
+                      />
+                    </div>
+                  )
+                })
+              )}
+              <div ref={observerElem}>
+                {isFetchingNextPage && <ReviewSkeleton />}
               </div>
-            )
-          })}
+            </>
+          ) : (
+            <p>값 없어</p>
+          )}
         </div>
       </main>
     </div>
