7c273ae94a5000a0063332979ee5191c34cb3a6d
junbeom-Son <junbeom247@gmail.com>
2024-10-05 21:53:41 +0900

    chore: remove unused import statements

--- backend/push-alert/src/main/java/io/ssafy/p/j11a307/push_alert/util/FirebaseUtil.java
+++ backend/push-alert/src/main/java/io/ssafy/p/j11a307/push_alert/util/FirebaseUtil.java
@@ -1,31 +1,20 @@
 package io.ssafy.p.j11a307.push_alert.util;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
 import com.google.firebase.messaging.FirebaseMessaging;
 import com.google.firebase.messaging.FirebaseMessagingException;
 import com.google.firebase.messaging.Message;
-import io.ssafy.p.j11a307.push_alert.dto.FcmAlert;
-import io.ssafy.p.j11a307.push_alert.dto.FcmMessage;
-import io.ssafy.p.j11a307.push_alert.dto.FcmNotification;
 import io.ssafy.p.j11a307.push_alert.dto.alerts.FcmAlertData;
 import io.ssafy.p.j11a307.push_alert.exception.BusinessException;
 import io.ssafy.p.j11a307.push_alert.exception.ErrorCode;
 import jakarta.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.ClassPathResource;
-import org.springframework.http.*;
-import org.springframework.http.converter.StringHttpMessageConverter;
 import org.springframework.stereotype.Component;
-import org.springframework.web.client.HttpClientErrorException;
-import org.springframework.web.client.RestTemplate;
 
 import java.io.IOException;
-import java.nio.charset.StandardCharsets;
-import java.util.List;
 
 @Component
 public class FirebaseUtil {
