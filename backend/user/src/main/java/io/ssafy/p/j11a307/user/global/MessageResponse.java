6ae510f2c1555652af219395c47657e13f447c5f
junbeom-Son <junbeom247@gmail.com>
2024-10-06 18:17:12 +0900

    feat: create global responses

---
+++ backend/user/src/main/java/io/ssafy/p/j11a307/user/global/MessageResponse.java
@@ -0,0 +1,21 @@
+package io.ssafy.p.j11a307.user.global;
+
+import lombok.Getter;
+
+/**
+ * 데이터 없이 응답 메시지만 필요한 경우 사용하는 응답 형태
+ */
+@Getter
+public class MessageResponse {
+    private final String message;
+
+    // 생성자: 메시지 전달
+    private MessageResponse(String message) {
+        this.message = message;
+    }
+
+    // 메시지를 설정하는 정적 팩토리 메서드
+    public static MessageResponse of(String message) {
+        return new MessageResponse(message);
+    }
+}
