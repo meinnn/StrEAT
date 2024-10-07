d1a63fc48d8c726085fe8087ba43d81147c2f997
이주호 <jeuslee99@naver.com>
2024-10-07 17:17:17 +0900

    feat: docker-compose 업데이트 및 Test 코드 어노테이션 삭제

--- backend/announcement/announcement/src/test/java/io/ssafy/p/j11a307/announcement/AnnouncementApplicationTests.java
+++ backend/announcement/announcement/src/test/java/io/ssafy/p/j11a307/announcement/AnnouncementApplicationTests.java
@@ -3,7 +3,6 @@ package io.ssafy.p.j11a307.announcement;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class AnnouncementApplicationTests {
 
 	@Test
