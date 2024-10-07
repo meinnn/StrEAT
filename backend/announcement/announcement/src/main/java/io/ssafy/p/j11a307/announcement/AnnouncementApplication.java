c57e8165f305473d4b321e91776c9378eced5d73
이주호 <jeuslee99@naver.com>
2024-10-07 16:42:32 +0900

    feat: announcement 마이크로서비스 환경 구축

---
+++ backend/announcement/announcement/src/main/java/io/ssafy/p/j11a307/announcement/AnnouncementApplication.java
@@ -0,0 +1,13 @@
+package io.ssafy.p.j11a307.announcement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnnouncementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnnouncementApplication.class, args);
+	}
+
+}
