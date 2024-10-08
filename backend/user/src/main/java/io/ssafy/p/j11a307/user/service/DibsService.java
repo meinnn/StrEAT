7bb13fe0f793ab1ae335664d5a45c99667d5d0b5
junbeom-Son <junbeom247@gmail.com>
2024-10-08 13:05:13 +0900

    feat: add store name on response

--- backend/user/src/main/java/io/ssafy/p/j11a307/user/service/DibsService.java
+++ backend/user/src/main/java/io/ssafy/p/j11a307/user/service/DibsService.java
@@ -1,5 +1,6 @@
 package io.ssafy.p.j11a307.user.service;
 
+import io.ssafy.p.j11a307.user.dto.DibsStoreStatusResponse;
 import io.ssafy.p.j11a307.user.dto.StoreDibsResponse;
 import io.ssafy.p.j11a307.user.entity.Subscription;
 import io.ssafy.p.j11a307.user.exception.BusinessException;
@@ -10,7 +11,10 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -21,6 +25,7 @@ public class DibsService {
 
     private final UserService userService;
     private final FcmService fcmService;
+    private final StoreService storeService;
 
     private final SubscriptionRepository subscriptionRepository;
 
@@ -65,9 +70,20 @@ public class DibsService {
         }
         // store이름, 별점 구해오는 api 연결하기
         List<Subscription> subscriptions = subscriptionRepository.findBySubscriptionIdUserId(userId);
+        // store Id 추출
+        List<Integer> storeIds = subscriptions.stream().map(Subscription::getSubscriptionId).map(Subscription.SubscriptionId::getStoreId).toList();
+
+        // store server에서 store id, store name, status 추출
+        List<DibsStoreStatusResponse> dibsStoreStatusResponse = storeService.getStoreStatusByIds(storeIds);
+
+        // store id -> name, status를 저장한 객체를 가리킨 Map 생성
+        Map<Integer, DibsStoreStatusResponse> storeIdToDibsStoreStatusResponse =
+                dibsStoreStatusResponse.stream().collect(Collectors.toMap(DibsStoreStatusResponse::storeId, store -> store));
+
         List<StoreDibsResponse> storeDibsResponses = subscriptions.stream().map(subscription ->
                 StoreDibsResponse.builder()
                         .storeId(subscription.getSubscriptionId().getStoreId())
+                        .storeName(storeIdToDibsStoreStatusResponse.get(subscription.getSubscriptionId().getStoreId()).name())
                         .alertOn(subscription.getAlertOn()).build()).toList();
         return storeDibsResponses;
     }
