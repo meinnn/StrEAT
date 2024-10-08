d285f5bb90bb09025457e1ddcbec296da2d8e040
junbeom-Son <junbeom247@gmail.com>
2024-10-08 14:19:04 +0900

    fix: modify http method

--- backend/payment/src/main/java/io/ssafy/p/j11a307/payment/controller/PaymentController.java
+++ backend/payment/src/main/java/io/ssafy/p/j11a307/payment/controller/PaymentController.java
@@ -10,7 +10,10 @@ import io.ssafy.p.j11a307.payment.service.PaymentService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
@@ -18,7 +21,7 @@ public class PaymentController {
 
     private final PaymentService paymentService;
 
-    @GetMapping("/toss/request-payment")
+    @PostMapping("/toss/request-payment")
     public ResponseEntity<DataResponse<PaymentResponse>> tossRequestPayment(TossPaymentBaseRequest tossPaymentBaseRequest) throws JsonProcessingException {
         PaymentResponse paymentResponse = paymentService.tossRequestPayment(tossPaymentBaseRequest);
         return ResponseEntity.status(HttpStatus.OK).body(DataResponse.of("결제 성공", paymentResponse));
