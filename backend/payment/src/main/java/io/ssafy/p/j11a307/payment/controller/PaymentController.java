89d30554aaa35d59c330564e55c901db2292bc9f
junbeom-Son <junbeom247@gmail.com>
2024-10-08 14:28:46 +0900

    docs: add swagger ui on controller method

--- backend/payment/src/main/java/io/ssafy/p/j11a307/payment/controller/PaymentController.java
+++ backend/payment/src/main/java/io/ssafy/p/j11a307/payment/controller/PaymentController.java
@@ -7,6 +7,13 @@ import io.ssafy.p.j11a307.payment.dto.TossPaymentCancelRequest;
 import io.ssafy.p.j11a307.payment.global.DataResponse;
 import io.ssafy.p.j11a307.payment.global.MessageResponse;
 import io.ssafy.p.j11a307.payment.service.PaymentService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.Parameters;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -22,12 +29,31 @@ public class PaymentController {
     private final PaymentService paymentService;
 
     @PostMapping("/toss/request-payment")
+    @Operation(summary = "토스 결제 요청", description = "토스 결제 요청")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "토스 결제 요청",
+                    content = @Content(mediaType = "application/json", schema = @Schema(implementation = PaymentResponse.class))),
+            @ApiResponse(responseCode = "404", description = "해당 id 유저 없음")
+    })
+    @Parameters({
+            @Parameter(name = "paymentKey", description = "toss payments가 제공하는 paymentKey"),
+            @Parameter(name = "orderId", description = "order id"),
+            @Parameter(name = "amount", description = "결제 금액")
+    })
     public ResponseEntity<DataResponse<PaymentResponse>> tossRequestPayment(@RequestBody TossPaymentBaseRequest tossPaymentBaseRequest) throws JsonProcessingException {
         PaymentResponse paymentResponse = paymentService.tossRequestPayment(tossPaymentBaseRequest);
         return ResponseEntity.status(HttpStatus.OK).body(DataResponse.of("결제 성공", paymentResponse));
     }
 
     @PostMapping("/toss/cancel-payment/{orderId}")
+    @Operation(summary = "토스 결제 취소 요청", description = "토스 결제 취소 요청")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "토스 결제 취소 요청"),
+            @ApiResponse(responseCode = "404", description = "해당 id 유저 없음")
+    })
+    @Parameters({
+            @Parameter(name = "cancelReason", description = "취소 사유")
+    })
     public ResponseEntity<MessageResponse> cancelTossPayment(
             @PathVariable Integer orderId, @RequestBody TossPaymentCancelRequest tossPaymentCancelRequest) {
 
