06213a3ceb386d6d41679f56a4098c1074e7166a
junbeom-Son <junbeom247@gmail.com>
2024-10-08 16:25:29 +0900

    feat: get owner information

--- backend/user/src/main/java/io/ssafy/p/j11a307/user/dto/OwnerProfile.java
+++ backend/user/src/main/java/io/ssafy/p/j11a307/user/dto/OwnerProfile.java
@@ -1,6 +1,9 @@
 package io.ssafy.p.j11a307.user.dto;
 
+import io.ssafy.p.j11a307.user.entity.Owner;
+
 public record OwnerProfile(
+        String ownerName,
         String gender,
         Integer age,
         String birth,
@@ -9,4 +12,18 @@ public record OwnerProfile(
         String phone,
         String email,
         String snsAccount
-) {}
+) {
+    public OwnerProfile(Owner owner) {
+        this(
+                owner.getUsername(),
+                owner.getGender(),
+                owner.getAge(),
+                owner.getBirth(),
+                owner.getAddress(),
+                owner.getTel(),
+                owner.getPhone(),
+                owner.getEmail(),
+                owner.getSnsAccount()
+        );
+    }
+}
