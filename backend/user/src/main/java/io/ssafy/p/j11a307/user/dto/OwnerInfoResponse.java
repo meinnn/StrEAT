fc613221104f363d5d100f99f45e03650a87132d
junbeom-Son <junbeom247@gmail.com>
2024-10-09 15:55:16 +0900

    feat: get owner info

---
+++ backend/user/src/main/java/io/ssafy/p/j11a307/user/dto/OwnerInfoResponse.java
@@ -0,0 +1,19 @@
+package io.ssafy.p.j11a307.user.dto;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Builder;
+
+@Schema(description = "사장 정보")
+@Builder
+public record OwnerInfoResponse(
+        @Schema(description = "이름", example = "김유경")
+        String name,
+        @Schema(description = "프로필 사진 경로", example = "http://k.kakaocdn.net/../img_110x110.jpg")
+        String profileImgSrc,
+        @Schema(description = "주문 상태 알림 설정 정보", example = "true / false")
+        Boolean orderStatusAlert,
+        @Schema(description = "찜 가게 오픈 알림 설정 정보", example = "true / false")
+        Boolean dibsStoreAlert,
+        @Schema(description = "사장이 운영하는 점포 id", example = "22")
+        Integer storeId
+) {}
