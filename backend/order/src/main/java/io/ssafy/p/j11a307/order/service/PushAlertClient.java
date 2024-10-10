707b9dfc9c45e2474cc9b29bdd5e4be1dec4a6b7
junbeom-Son <junbeom247@gmail.com>
2024-10-11 05:53:05 +0900

    fix: convert to post mapping

--- backend/order/src/main/java/io/ssafy/p/j11a307/order/service/PushAlertClient.java
+++ backend/order/src/main/java/io/ssafy/p/j11a307/order/service/PushAlertClient.java
@@ -2,22 +2,23 @@ package io.ssafy.p.j11a307.order.service;
 
 import io.ssafy.p.j11a307.order.dto.OrderStatusChangeRequest;
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @FeignClient(name = "push-alert")
 public interface PushAlertClient {
 
-    @GetMapping("/api/push-alert/order-accept")
+    @PostMapping("/api/push-alert/order-accept")
     void sendOrderAcceptedAlert(@RequestParam OrderStatusChangeRequest orderStatusChangeRequest,
                                     @RequestHeader(value = "X-Internal-Request") String internalRequest);
 
-    @GetMapping("/api/push-alert/cooking-completed")
-    void sendCookingCompletedAlert(@RequestParam OrderStatusChangeRequest orderStatusChangeRequest,
+    @PostMapping("/api/push-alert/cooking-completed")
+    void sendCookingCompletedAlert(@RequestBody OrderStatusChangeRequest orderStatusChangeRequest,
                                    @RequestHeader(value = "X-Internal-Request") String internalRequest);
 
-    @GetMapping("/api/push-alert/pickup-completed")
+    @PostMapping("/api/push-alert/pickup-completed")
     void sendPickupCompletedAlert(@RequestParam OrderStatusChangeRequest orderStatusChangeRequest,
                                    @RequestHeader(value = "X-Internal-Request") String internalRequest);
 }
