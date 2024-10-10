49f74195a128807a8a7f2e5a80bfbda370dcd209
dana019 <dana019@naver.com>
2024-10-11 06:18:40 +0900

    feat: 포그라운드 알림 설정 변경

--- front/public/firebase-messaging-sw.js
+++ front/public/firebase-messaging-sw.js
@@ -14,12 +14,24 @@ const firebaseConfig = {
 firebase.initializeApp(firebaseConfig)
 const messaging = firebase.messaging()
 
-// messaging.onBackgroundMessage((payload) => {
-//   const title = `${payload.notification?.title} background`
-//   const notificationOptions = {
-//     body: payload.notification.body,
-//     icon: '/web-app-manifest-192x192.png',
-//   }
-//
-//   self.registration.showNotification(title, notificationOptions)
-// })
+messaging.onBackgroundMessage((payload) => {
+  const title = payload.notification?.title || 'Default Title'
+  const notificationOptions = {
+    body: payload.notification.body,
+    icon: '/web-app-manifest-192x192.png',
+    data: {
+      url: payload.notification?.click_action || '/', // 알림 클릭 시 이동할 URL 설정
+    },
+  }
+
+  self.registration.showNotification(title, notificationOptions)
+})
+
+self.addEventListener('notificationclick', function (event) {
+  const url = event.notification.data.url || '/' // 알림에 설정된 URL이 없다면 기본 루트로 이동
+  event.notification.close() // 알림 닫기
+
+  event.waitUntil(
+    clients.openWindow(url) // 새로운 창이나 탭으로 이동
+  )
+})
