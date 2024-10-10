7f5c0bc04387a60b71d38da7f31842e35dde991f
junbeom-Son <junbeom247@gmail.com>
2024-10-11 02:03:39 +0900

    refactor: modify order of if statements

--- backend/order/src/main/java/io/ssafy/p/j11a307/order/service/OrderService.java
+++ backend/order/src/main/java/io/ssafy/p/j11a307/order/service/OrderService.java
@@ -122,23 +122,26 @@ public class OrderService {
         Integer ownerId = ownerClient.getOwnerId(token, internalRequestKey);
 
         //1. 주문 내역이 존재하지 않는다면?
-        Optional<Orders> orders = ordersRepository.findById(ordersId);
+        Orders orders = ordersRepository.findById(ordersId).orElseThrow(() -> new BusinessException(ErrorCode.ORDER_NOT_FOUND));
 
-        if(orders.isPresent()) {
-            //2. 처리할 권한이 없다면?
-            ReadStoreDTO readStoreDTO = storeClient.getStoreInfo(orders.get().getStoreId()).getData();
-            if(ownerId != readStoreDTO.userId()) throw new BusinessException(ErrorCode.UNAUTHORIZED_USER);
+        //2. 처리할 권한이 없다면?
+        ReadStoreDTO readStoreDTO = storeClient.getStoreInfo(orders.getStoreId()).getData();
+        if(!ownerId.equals(readStoreDTO.userId())) {
+            throw new BusinessException(ErrorCode.UNAUTHORIZED_USER);
+        }
 
-            //3. 내역이 대기 중인 상태가 아니라면?
-            if(orders.get().getStatus().equals(OrderCode.WAITING_FOR_PROCESSING)) {
-                if (flag == 0) orders.get().updateStatus(OrderCode.REJECTED);
-                else if(flag == 1) orders.get().updateStatus(OrderCode.PROCESSING);
-                else throw new BusinessException(ErrorCode.WRONG_FLAG);
+        //3. 내역이 대기 중인 상태가 아니라면?
+        if(!orders.getStatus().equals(OrderCode.WAITING_FOR_PROCESSING)) {
+            throw new BusinessException(ErrorCode.WRONG_ORDER_ID);
+        }
 
-                ordersRepository.save(orders.get());
-
-            } else throw new BusinessException(ErrorCode.WRONG_ORDER_ID);
-        } else throw new BusinessException(ErrorCode.ORDER_NOT_FOUND);
+        if (flag == 0) {
+            orders.updateStatus(OrderCode.REJECTED);
+        }
+        else if(flag == 1) {
+            orders.updateStatus(OrderCode.PROCESSING);
+        }
+        throw new BusinessException(ErrorCode.WRONG_FLAG);
     }
 
     @Transactional
